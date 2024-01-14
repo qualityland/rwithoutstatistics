4048d8ad4d34cd6a79c953142586f50956348c73
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2024-01-14 11:03:51 +0100

    midth of chapter 2

--- 02_Principles_of_Data_Vis.R
+++ 02_Principles_of_Data_Vis.R
@@ -3,6 +3,20 @@ library(tidyverse)
 gapminder_10_rows <- read_csv("https://data.rwithoutstatistics.com/gapminder_10_rows.csv")
 
 
+## First Layer: Mapping Data to Aesthetic Properties
+
+ggplot(
+  data = gapminder_10_rows,
+  mapping = aes(
+    x = year,
+    y = lifeExp
+  )
+)
+
+
+
+## Second Layer: Choosing the geoms
+
 # scatter plot
 ggplot(
   data = gapminder_10_rows,
@@ -13,7 +27,6 @@ ggplot(
 ) +
   geom_point()
 
-
 # line plot
 ggplot(
   data = gapminder_10_rows,
@@ -23,7 +36,6 @@ ggplot(
   )
 ) +
   geom_line()
-
 
 # line & points
 ggplot(
@@ -36,7 +48,6 @@ ggplot(
   geom_point() +
   geom_line()
 
-
 # column chart
 ggplot(
   data = gapminder_10_rows,
@@ -48,3 +59,44 @@ ggplot(
   geom_col()
 
 
+
+## Third Layer: Altering Aesthetic Properties
+
+# fill columns
+ggplot(
+  data = gapminder_10_rows,
+  mapping = aes(
+    x = year,
+    y = lifeExp,
+    fill = year
+  )
+) +
+  geom_col()
+
+# alter fill
+ggplot(
+  data = gapminder_10_rows,
+  mapping = aes(
+    x = year,
+    y = lifeExp,
+    fill = year
+  )
+) +
+  geom_col() +
+  scale_fill_viridis_c()
+
+
+
+## Fourth Layer: Setting a Theme
+
+ggplot(
+  data = gapminder_10_rows,
+  mapping = aes(
+    x = year,
+    y = lifeExp,
+    fill = year
+  )
+) +
+  geom_col() +
+  scale_fill_viridis_c() +
+  theme_minimal()
